ae7a8796318f9c6de25e0d481599a001a60f6ff3
Jandy <reverdy@protonmail.com>
2019-10-24 23:53:01 -0700

    scarycard basics complete

--- halloweenclicky/src/App.js
+++ halloweenclicky/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+// import './Images'
+import img1 from './Images/1.jpg';
+import img2 from './Images/2.jpg';
+import img3 from './Images/3.jpg';
+import img4 from './Images/4.jpg';
+import img5 from './Images/5.jpg';
+import img6 from './Images/6.jpg';
+import img7 from './Images/7.jpg';
+import img8 from './Images/8.jpg';
+import img9 from './Images/9.jpg';
+import img10 from './Images/10.jpg';
+import img11 from './Images/11.jpg';
+import img12 from './Images/12.jpg';
+import ScaryCards from './Components/ScaryCards.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <ScaryCards src={img1} />
 
       </header>
     </div>
--- halloweenclicky/src/Components/ScaryCards.js/index.js
+++ halloweenclicky/src/Components/ScaryCards.js/index.js
@@ -1,1 +1,12 @@
 import React from "react";
+import "./style.css";
+
+export default function ScaryCard(props) {
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt="images" src={props.src} />
+            </div>
+        </div>
+    )
+}
