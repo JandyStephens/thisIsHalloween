5d2815743f2b65021f9b245fc52c87521ec46dc0
Jandy <reverdy@protonmail.com>
2019-10-25 00:17:47 -0700

    mapped/rendered all images & set beginning state

--- halloweenclicky/src/App.js
+++ halloweenclicky/src/App.js
@@ -15,15 +15,41 @@ import img11 from './Images/11.jpg';
 import img12 from './Images/12.jpg';
 import ScaryCards from './Components/ScaryCards.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <ScaryCards src={img1} />
+const images = [
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12
+]
 
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    score: 0,
+    totalScore: 0,
+    imgs: images
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* <ScaryCards src={img1} /> */}
+          {images.map(image => {
+            return <ScaryCards src={image} />
+          })}
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
